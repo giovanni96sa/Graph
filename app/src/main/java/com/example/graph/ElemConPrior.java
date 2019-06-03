7f0ef5202020cee3efc54a7cbf323426d60ef376
Giovanni <giovannicammarano1996@gmail.com>
2019-06-03 11:14:23 +0200

    secomdo commit

---
+++ app/src/main/java/com/example/graph/ElemConPrior.java
@@ -0,0 +1,11 @@
+package com.example.graph;
+public class ElemConPrior<V> {
+	
+	V element;
+	double priority;
+	ElemConPrior(V el,double p){
+		element=el;
+		priority=p;
+	
+	}
+}
---
+++ app/src/main/java/com/example/graph/PriorityQueue.java
@@ -0,0 +1,10 @@
+package com.example.graph;
+
+public interface PriorityQueue<V> {
+	boolean isEmpty();
+	void insert(V element, double priority);
+	V extractfirst();
+	V getFirst();
+	boolean decreasePriority(V element, double newPriority);
+	void remove(V element);
+}
